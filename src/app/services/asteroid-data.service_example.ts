49d3d7068cb13dffe2437ecbc3c3e2a41f531871
BrandonSteffan <brandon.steffan15@gmail.com>
2023-10-22 19:02:06 +0200

    updated gitignore

--- src/app/services/asteroid-data.service_example.ts
+++ src/app/services/asteroid-data.service_example.ts
@@ -11,14 +11,16 @@ export class AsteroidDataService {
   constructor(private httpClient: HttpClient) { }
   getAsteroidData(): Observable<asteroid_response_body>{
     const asteroidUrl = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-09-25&end_date=2023-10-2&";
-    const apiKey = "api_key=rXXV4rb59A2CTgI46wwmc5PbVqFQM44DMlWrKRK7";
+    //Insert your api key here
+    const apiKey = "api_key=";
     const url = asteroidUrl + apiKey;
     return this.httpClient.get<asteroid_response_body>(url);
   }
 
   getAsteroidById(id:number):Observable<Asteroid>{
     const asteroidUrl = "https://api.nasa.gov/neo/rest/v1/neo/";
-    const apiKey = "api_key=rXXV4rb59A2CTgI46wwmc5PbVqFQM44DMlWrKRK7"
+    //Insert your api key here
+    const apiKey = "api_key="
 
     const url = asteroidUrl+ id +"?" +apiKey;
 
